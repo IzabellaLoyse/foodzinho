b95015f0ca511c5e5bdc7a352a74c078a338657c
Izabella Loyse <izabellaloyse13@gmail.com>
2022-06-17 01:41:05 -0300

    feat(header): created header component

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,7 @@
-// JavaScript
+const navItemsElement = document.querySelectorAll('.js-nav-item a');
+
+navItemsElement.forEach((item) => {
+  item.addEventListener('click', () => {
+    item.classList.toggle('is-header__link--active');
+  });
+});
